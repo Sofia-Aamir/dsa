a07887a27c732dea72fe44ae5ae33ac727ce3e32
Masoodkhan5933 <Masoodkhan5933@gmail.com>
2023-04-28 23:13:44 +0500

    check

---
+++ DSAcircular/src/linkedlist.java
@@ -0,0 +1,46 @@
+
+public class linkedlist {
+     node head;
+    node tail;
+    node temp;
+    
+     linkedlist(){
+        head=null;
+        tail=null;
+        temp=null;
+    }
+     
+       public void display() {
+        node p = head;
+        while (p != null) {
+            System.out.print(" " + p.data);
+            p = p.next;
+        }
+    }
+       
+      
+       public void insertAtStart(int data){
+            node newnode=new node(data);
+        
+        if(head==null){
+           head=newnode;
+          // tail=newnode;
+          // temp=newnode;
+          head.next=head;
+          head.previous=head;
+        }
+        
+        else{
+//            temp=head.next;
+//            newnode.next=temp;
+//            newnode.previous=tail;
+//            temp.previous=newnode;
+//            tail.next=newnode;
+
+              newnode.previous=head.previous;
+              newnode.next=head;
+              head.previous=newnode;
+              head=newnode;
+        }
+       }
+}
---
+++ DSAcircular/src/node.java
@@ -0,0 +1,12 @@
+
+public class node {
+     int data;
+    node next;
+    node previous;
+    
+    node (int data){
+        this.data=data;
+        next=null;
+        previous=null;
+    }
+}
